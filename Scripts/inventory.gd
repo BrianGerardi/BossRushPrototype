06c52cce2e250add0ca4a1a7f7373b0782500c54
BrianGerardi <briangerardi48@gmail.com>
2025-09-09 16:04:38 -0300

    agregadoInventarioDeItems

--- Scripts/inventory.gd
+++ Scripts/inventory.gd
@@ -1,28 +1,10 @@
 extends Control
-@onready var grid = $NinePatchRect/GridContainer
-var ItemScene = preload("res://Escenas/item.tscn")
 
-# Ejemplo de "objetos del jugador"
-var objetos = [
-	{"icono": preload("res://UIAssetsTest/png/Star/Medium/Active.png"), "nombre": "Espada", "cantidad": 1},
-	{"icono": preload("res://UIAssetsTest/png/Star/Medium/Unactive.png"), "nombre": "Poción", "cantidad": 3}
-]
+@export var description : NinePatchRect
 
+func set_description(item : Item):
+	description.get_node("Name").text = item.name
+	description.get_node("Icon").texture = item.icon
+	description.get_node("ItemDescription").text = item.itemdescription
 func _ready():
-	mostrar_inventario()
-
-func mostrar_inventario():
-	# Limpiar antes de rellenar
-	for child in grid.get_children():
-		child.queue_free()
-	
-	# Instanciar un Item.tscn por cada objeto
-	for data in objetos:
-		var item = ItemScene.instantiate()
-		item.icono = data["icono"]
-		item.nombre = data["nombre"]
-		item.cantidad = data["cantidad"]
-		grid.add_child(item)
-
-func _on_cerrar_button_pressed():
-	self.visible = false
+	pass
--- Scripts/item.gd
+++ Scripts/item.gd
@@ -1,12 +1,6 @@
-extends Control
+extends Resource
+class_name Item
 
-@export var icono: Texture
-@export var nombre: String = "Item"
-@export var cantidad: int = 1
-
-@onready var icono_sprite = $TextureRect2/TextureRect
-@onready var nombre_label = $TextureRect2/Label
-
-func _ready():
-	icono_sprite.texture = icono
-	nombre_label.text = nombre
+@export var name : String
+@export var icon : Texture2D
+@export_multiline var itemdescription : String
