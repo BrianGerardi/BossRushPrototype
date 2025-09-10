ef02d9587b1fa2bddbeef4ca8e3d6f7a76c14419
Marcos Nicolas Salinas <smarcosnicolas@gmail.com>
2025-09-10 01:56:32 -0300

    a

---
+++ Scripts/global.gd
@@ -0,0 +1,19 @@
+extends Node
+
+
+var posicion_player
+
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func get_posicion_player():
+	return posicion_player
+
+func set_posicion_player():
+	pass
