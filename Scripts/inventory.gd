9de4a2a5ecf22f00a9145d3d29678ebd605de050
Marcos Nicolas Salinas <smarcosnicolas@gmail.com>
2025-09-12 22:17:09 -0300

    solucionando bugs raros

--- Scripts/inventory.gd
+++ Scripts/inventory.gd
@@ -6,6 +6,6 @@ func set_description(item : Item):
 	description.get_node("Name").text = item.name
 	description.get_node("Icon").texture = item.icon
 	description.get_node("ItemDescription").text = item.itemdescription
-	description.get_node("LabelPrecio").text = str(item.precio)
+#	description.get_node("LabelPrecio").text = str(item.precio)
 func _ready():
 	pass
